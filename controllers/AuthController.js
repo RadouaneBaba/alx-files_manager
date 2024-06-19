be9d2dc53b4a28b9dcdd89304a7c225129a529e6
RadouaneBaba <radoine.baba04@gmail.com>
2024-06-19 10:17:44 -0700

    auth

---
+++ controllers/AuthController.js
@@ -0,0 +1,35 @@
+import sha1 from 'sha1';
+import { v4 as uuidv4 } from 'uuid';
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+const AuthController = {
+  getConnect: async (req, res) => {
+   const auth = req.headers['authorization'];
+   const cred = atob(auth.split(' ')[1]);
+   const email = cred.split(':')[0];
+   const password = sha1(cred.split(':')[1]);
+
+   const database = process.env.DB_DATABASE || 'files_manager';
+   const coll = await dbClient.client.db(database).collection('users');
+   const user = await coll.findOne({ email, password });
+
+   if (!user) return res.status(401).json({ error: 'Unauthorized' });
+
+   const token = uuidv4();
+   await redisClient.set(`auth_${token}`, user._id.toString(), 24 * 3600);
+   return res.json({ token });
+  },
+  getDisconnect: async (req, res) => {
+    const token = `auth_${req.headers['x-token']}`;
+    const userId = await redisClient.get(token);
+
+    if (!userId) return res.status(401).json({ error: 'Unauthorized' });
+
+    await redisClient.del(token);
+	
+    return res.sendStatus(204);
+  },
+};
+
+export default AuthController;
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,7 @@
 import sha1 from 'sha1';
+import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 const UsersController = {
   postNew: async (req, res) => {
@@ -12,6 +14,17 @@ const UsersController = {
     if (user) return res.status(400).json({ error: 'Already exist' });
     const result = await coll.insertOne({ email, password: sha1(password) });
     return res.status(201).json({ email, id: result.insertedId });
+  },
+  getMe: async(req, res) => {
+    const token = `auth_${req.headers['x-token']}`;
+    const userId = await redisClient.get(token);
+
+    if (!userId) return res.status(401).json({ error: 'Unauthorized' });
+    
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const coll = await dbClient.client.db(database).collection('users');
+    const user = await coll.findOne({ _id: ObjectId(userId) });
+    res.json({ id: user._id, email: user.email });
   },
 };
 
