4d5d46644649bf99b8d06a5ec0bf4c53c9d83ac1
RadouaneBaba <radoine.baba04@gmail.com>
2024-06-19 15:13:47 -0700

    fix

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -9,7 +9,7 @@ const AuthController = {
     const cred = Buffer.from(auth.split(' ')[1], 'base64').toString('utf8');
     if (!cred) return res.status(401).json({ error: 'Unauthorized' });
     const email = cred.split(':')[0];
-    const password = sha1(cred.split(':')[1]); 
+    const password = sha1(cred.split(':')[1]);
     const coll = await dbClient.db.collection('users');
     const user = await coll.findOne({ email, password });
 
