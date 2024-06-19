5259a9bfd03af04481bea7717adad7f756b98055
RadouaneBaba <radoine.baba04@gmail.com>
2024-06-19 08:22:13 -0700

    test3

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,17 +1,17 @@
-import dbClient from '../utils/db';
 import sha1 from 'sha1';
+import dbClient from '../utils/db';
 
 const UsersController = {
   postNew: async (req, res) => {
     const { email, password } = req.body;
     if (!email) return res.status(400).json({ error: 'Missing email' });
-    if (!password) return res.status(400).json({ error: 'Missing password' });  
+    if (!password) return res.status(400).json({ error: 'Missing password' });
     const database = process.env.DB_DATABASE || 'files_manager';
     const coll = await dbClient.client.db(database).collection('users');
-    const user = await coll.findOne({ email: email });
+    const user = await coll.findOne({ email });
     if (user) return res.status(400).json({ error: 'Already exist' });
-    const result = await coll.insertOne({email: email, password: sha1(password) });
-    return res.status(201).json({ email: email, id: result.insertedId });
+    const result = await coll.insertOne({ email, password: sha1(password) });
+    return res.status(201).json({ email, id: result.insertedId });
   },
 };
 
